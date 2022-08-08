c24acc40aac5483c5569434a7a40048283d39440
matan <you@example.com>
2022-08-08 18:36:00 +0300

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+	const [loading, setLoading] = useState(false);
+	const [tours, setTours] = useState([]);
+
+	const getTours = async () => {
+		setLoading(true);
+		try {
+			const res = await fetch(url);
+			const data = await res.json();
+			setTours(data);
+			setLoading(false);
+		} catch (error) {
+      console.error('Getting data failed');
+    }
+	};
+  console.log(tours);
+	useEffect(() => {
+		getTours();
+	}, []);
+
+
+	if (loading) {
+		return (
+			<main>
+				<Loading />
+			</main>
+		);
+	}
+	return (
+		<main>
+			<Tours data={tours} />
+		</main>
+	);
 }
 
-export default App
+export default App;
--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,20 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ data }) => {
+  console.log(data);
+	return (
+		<section>
+			<div className="title">
+				<h2>Ours Tours</h2>
+				<div className="underline"></div>
+			</div>
+			<div>
+				{data.map((item) => {
+					return <Tour key={item.id} {...item}></Tour>
+				})}
+			</div>
+		</section>
+	);
 };
 
 export default Tours;
